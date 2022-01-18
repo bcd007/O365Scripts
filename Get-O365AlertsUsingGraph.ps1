ee98634c72bd84b3075d53620bd430e9688899ff
bcd007 <bdillon007@gmail.com>
2022-01-18 08:10:09 -0500

    New version of Alert script that correctly gets more than the initial 100 results

--- Get-O365AlertsUsingGraph.ps1
+++ Get-O365AlertsUsingGraph.ps1
@@ -5,12 +5,13 @@ This script will retrieve the list of current O365 Service Alerts from GraphAPI,
 .DESCRIPTION
 This script will retrieve the list of current O365 Service Alerts with a status of 'serviceDegradation' from GraphAPI, and update a SPO/Teams List with the alert data
 This script will also remove any items in the SPO list where the Status of the Alert has changed from 'serviceDegradation' to any other status
-Note:  This script uses only GraphAPI, no other modules are required.
+Note:   This script uses only GraphAPI, no other modules are required.
+        This script requires a json file with the AppID Key and encrypted secret.  The script can be modified to support a certificate or Azure keystore
 
 The SPO list is a standard SharePoint Online list that has the following columns:
-title,id,impactDescription,classification,featureGroup,service,status,startDateTime
+title,alertID,impactDescription,classification,featureGroup,service,status,startDateTime
 
-This script requires an Azure AD ApplicationRegistration that has the following GraphAPI Application Permissions consented:
+This script requires an Azure AD ApplicationID that has the following GraphAPI Application Permissions added and consented:
      ServiceMessage.Read.All
      ServiceHealth.Read.All
      Sites.Manage.All
@@ -20,13 +21,13 @@ This script requires an Azure AD ApplicationRegistration that has the following 
 Date Written: 01/12/2022
 CR/SR/INC: N/A
 Author: Bob Dillon
-Version:  2.0
+Version:  1.0
 
 .EXAMPLE
 (Get-Content -Raw "C:\Temp\O365IssuesScriptData.json") | ConvertFrom-Json | C:\Temp\Get-O365AlertsUsingGraph.ps1
 
 .EXAMPLE
-Get-O365AlertsusingGraph.ps1 -tenantID <tenantID> -clientFile <Path and/or Filename of CredFile.json> -siteID <GUID of SharePoint Site> -listId <GUID of SPO List to write to>
+Get-O365AlertsUsingGraph.ps1 -tenantID (tenantID) -clientFile (Path and/or Filename of CredFile.json) -siteID (GUID of SharePoint Site> -listId <GUID of SPO List to write to)
 #>
 
 [CmdletBinding()]
@@ -58,7 +59,15 @@ $Header = @{
 $serviceHealthresults = @()
 $serviceStatusReturnURI = "https://graph.microsoft.com/v1.0/admin/serviceAnnouncement/issues"
 $SvcHealthAlerts = Invoke-RestMethod -Headers $Header -Uri $serviceStatusReturnURI -Method Get -ContentType "application/json"
-$serviceHealthresults = $SvcHealthAlerts.value | Select-Object -Property title,id,impactDescription,classification,featureGroup,service,status,startDateTime
+$serviceHealthresults = $SvcHealthAlerts.value
+
+# Get another 900 because Micrsoft hasn't figured out how to sort their alerts by date....yet.
+$count = 0
+Do{$count++ ; $serviceHealthresults += (Invoke-RestMethod -Uri $SvcHealthAlerts.'@odata.nextLink' -Headers $Header -Method Get -ContentType "application/json").Value}
+Until($count -eq 9)
+
+# Microsoft puts duplicate alerts in this report, lets get rid of them because 'dumb'
+$serviceHealthresults = $serviceHealthresults | Sort-Object -Property id -Unique
 
 # Get the List and List Items via Graph, and set up the Add List Item URI
 $listItemsURI = "https://graph.microsoft.com/v1.0/sites/$siteID/lists/$listID/items?expand=fields"
@@ -68,12 +77,12 @@ $listItemstoAddURI = "https://graph.microsoft.com/v1.0/sites/$siteID/lists/$list
 
 # Parse through each service alert and determine if the individual alert needs to be added to or removed from the List.
 ForEach($svcAlertToAnalyze in $serviceHealthresults){
-    If($currentListItems.fields.Id0 -notcontains $svcAlertToAnalyze.Id -And $svcAlertToAnalyze.Status -eq "serviceDegradation"){
+    If($currentListItems.fields.AlertID -notcontains $svcAlertToAnalyze.Id -And $svcAlertToAnalyze.Status -eq "serviceDegradation"){
         # Create a nested hashtable to match the JSON format/case needed to add a list item via GraphAPI
         $bodyToWrite = @{
             fields = 
                 @{Title = $svcAlertToAnalyze.title
-                Id0 = $svcAlertToAnalyze.id
+                AlertID = $svcAlertToAnalyze.id
                 impactDescription = $svcAlertToAnalyze.impactDescription
                 classification = $svcAlertToAnalyze.classification
                 featureGroup = $svcAlertToAnalyze.featureGroup
@@ -86,8 +95,8 @@ ForEach($svcAlertToAnalyze in $serviceHealthresults){
         $addnewlistItemResults = Invoke-RestMethod -Headers $Header -Uri $listItemstoAddURI -Method POST -ContentType "application/json" -Body ($bodyToWrite | ConvertTo-Json)
     }
     # Remove any list items where the status has changed from 'serviceDegradation' to any other status
-    If($currentListItems.fields.Id0 -contains $svcAlertToAnalyze.Id -And $svcAlertToAnalyze.Status -eq "serviceRestored"){
-        $currentlistitems | Where-Object{$_.fields.Id0 -eq $svcAlertToAnalyze.Id} | ForEach-Object{
+    If($currentListItems.fields.AlertID -contains $svcAlertToAnalyze.Id -And $svcAlertToAnalyze.Status -eq "serviceRestored"){
+        $currentlistitems | Where-Object{$_.fields.AlertID -eq $svcAlertToAnalyze.Id} | ForEach-Object{
             $deleteListItemURI = "https://graph.microsoft.com/v1.0/sites/$siteID/lists/$listID/items/$($_.id)"
             $listItemsTodelete = Invoke-RestMethod -Headers $Header -Uri $deleteListItemURI  -Method DELETE -ContentType "application/json"
         }
